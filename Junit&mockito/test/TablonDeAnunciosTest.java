cdd9dfa177441c7356cd9a9fbe7b4b8fb0f3bc9b
Carlos Melero <carlyto1520@gmail.com>
2016-05-26 17:09:06 +0200

    Primer test

--- Junit&mockito/test/TablonDeAnunciosTest.java
+++ Junit&mockito/test/TablonDeAnunciosTest.java
@@ -2,11 +2,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junio2013.TablonDeAnuncios;
+
 public class TablonDeAnunciosTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Test1() {
+		TablonDeAnuncios TDA = new TablonDeAnuncios();
+		assertEquals("No hay anuncios", 1, TDA.anunciosPublicados());
 	}
 
 }
