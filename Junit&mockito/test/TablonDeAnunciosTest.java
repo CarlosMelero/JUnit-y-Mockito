dcdb69933732e6b93bbaab89ecc5c359b0961d4e
Carlos Melero <carlyto1520@gmail.com>
2016-05-26 17:16:40 +0200

    Segundo Test y varios cambios en el codigo de Testing

--- Junit&mockito/test/TablonDeAnunciosTest.java
+++ Junit&mockito/test/TablonDeAnunciosTest.java
@@ -1,15 +1,29 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import junio2013.Anuncio;
+import junio2013.IBaseDeDatosDeAnunciantes;
+import junio2013.IBaseDeDatosDePagos;
 import junio2013.TablonDeAnuncios;
 
+
 public class TablonDeAnunciosTest {
+
+	private static final IBaseDeDatosDeAnunciantes IBaseDeDatosDeAnunciantes = null;
+	private static final IBaseDeDatosDePagos IBaseDeDatosDePagos = null;
 
 	@Test
 	public void Test1() {
 		TablonDeAnuncios TDA = new TablonDeAnuncios();
-		assertEquals("No hay anuncios", 1, TDA.anunciosPublicados());
+		assertEquals(1, TDA.anunciosPublicados());
 	}
 
+	@Test
+	public void Test2() {
+		TablonDeAnuncios TDA = new TablonDeAnuncios();
+		Anuncio A = new Anuncio("","","LA EMPRESA");
+		TDA.publicarAnuncio(A, IBaseDeDatosDeAnunciantes, IBaseDeDatosDePagos);
+		assertEquals(2, TDA.anunciosPublicados());
+	}
 }
