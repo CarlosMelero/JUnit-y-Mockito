4d6714684a7d535b50d773f49a9f5a7bbee98730
Carlos Melero <carlyto1520@gmail.com>
2016-05-27 09:42:37 +0200

    Tercer test y refactorizacion de codigo

--- Junit&mockito/test/TablonDeAnunciosTest.java
+++ Junit&mockito/test/TablonDeAnunciosTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import junio2013.Anuncio;
@@ -7,23 +11,41 @@ import junio2013.IBaseDeDatosDeAnunciantes;
 import junio2013.IBaseDeDatosDePagos;
 import junio2013.TablonDeAnuncios;
 
-
 public class TablonDeAnunciosTest {
+	TablonDeAnuncios TDA;
 
-	private static final IBaseDeDatosDeAnunciantes IBaseDeDatosDeAnunciantes = null;
-	private static final IBaseDeDatosDePagos IBaseDeDatosDePagos = null;
+	@Before
+	public void creaTablonInicial() {
+		TDA = new TablonDeAnuncios();
+	}
+
+	@After
+	public void eliminaTablonInicial() {
+		TDA = null;
+	}
 
 	@Test
 	public void Test1() {
-		TablonDeAnuncios TDA = new TablonDeAnuncios();
 		assertEquals(1, TDA.anunciosPublicados());
 	}
 
 	@Test
 	public void Test2() {
-		TablonDeAnuncios TDA = new TablonDeAnuncios();
-		Anuncio A = new Anuncio("","","LA EMPRESA");
-		TDA.publicarAnuncio(A, IBaseDeDatosDeAnunciantes, IBaseDeDatosDePagos);
+		Anuncio A = new Anuncio(" ", " ", "LA EMPRESA");
+		TDA.publicarAnuncio(A, null, null);
 		assertEquals(2, TDA.anunciosPublicados());
 	}
+
+	@Test
+	public void Test3() {
+		IBaseDeDatosDePagos pagos = mock(IBaseDeDatosDePagos.class);
+		IBaseDeDatosDeAnunciantes anunciantes = mock(IBaseDeDatosDeAnunciantes.class);
+		when(anunciantes.buscarAnunciante("Carlos")).thenReturn(true);
+		when(pagos.anuncianteTieneSaldo("Carlos")).thenReturn(false);
+		Anuncio A = new Anuncio(" ", " ", "Carlos");
+		TDA.publicarAnuncio(A, anunciantes, pagos);
+		assertEquals(TDA.anunciosPublicados(), 1);
+
+	}
+
 }
